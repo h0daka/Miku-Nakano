a28e5310254d54b68527d2237b1cc7c526260fdb
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-02-25 17:52:06 +0530

    Update __init__.py

--- MikuXProBot/modules/sql/__init__.py
+++ MikuXProBot/modules/sql/__init__.py
@@ -1,22 +1,15 @@
+from MikuXProBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from MikuXProBot import DB_URI, LOGGER, DEBUG
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8", echo=DEBUG)
-    LOGGER.info("[PostgreSQL] Connecting to database......")
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
-try:
-    SESSION: scoped_session = start()
-except Exception as e:
-    LOGGER.exception(f'[PostgreSQL] Failed to connect due to {e}')
-    exit()
-   
-LOGGER.info("[PostgreSQL] Connection successful, session started.")
+SESSION = start()
