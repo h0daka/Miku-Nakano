e12ca8672f95917d38c9ea99715384043c202096
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-02-26 11:21:16 +0530

    Create script.py

---
+++ MikuXProBot/script.py
@@ -0,0 +1,24 @@
+PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @MikuXProBot For Repo"""
+
+MIKU_IMG = (
+      "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
+      "https://telegra.ph//file/05311caed13176f7d35e5.jpg",
+      "https://telegra.ph//file/88976abda0d0af9d4a517.jpg",
+      "https://telegra.ph//file/b388f473ddfb9cc727bb1.jpg",
+      "https://telegra.ph//file/d950844dad2e473950470.jpg",
+)
+
+TEXXT = "*Do you Like me?*"
+
+MIKU_N_IMG = (
+      "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
+      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
+      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
+      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
+      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+
+)
+
+PM_PHOTO = "https://telegra.ph//file/3a88a958a88e83d85d284.jpg"
+
+MIKU_DISPACHER_PIC = "https://telegra.ph//file/4ce99db6136c77248e0ec.jpg"
