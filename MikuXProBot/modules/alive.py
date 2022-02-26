fc9643001765f789f013f2e37b799fa0e80f4f13
Tofu <91769994+Aditya6321@users.noreply.github.com>
2022-02-26 23:51:08 +0530

    Update alive.py

--- MikuXProBot/modules/alive.py
+++ MikuXProBot/modules/alive.py
@@ -86,4 +86,4 @@ async def hmm(yes):
 async def repo(event):
     Miku = f"**Hey [{event.sender.first_name}](tg://user?id={event.sender.id}), Click The Button Below To Get My Repo**\n\n"
     BUTTON = [[Button.url("[► Support ◄]", "https://t.me/MikusSupport"), Button.url("[► Repo ◄]", "https://github.com/h0daka/Miku-Nakano")]]
-    await borg.send_file(event.chat_id, file="https://telegra.ph/file/0f41f734003d3b24d81ef.jpg", caption=Miku, buttons=BUTTON)
+    await borg.send_file(event.chat_id, file="https://telegra.ph/file/57126f0464db84138b15b.jpg", caption=Miku, buttons=BUTTON)
