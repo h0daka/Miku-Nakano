e91f7a2db84cd30e550b07f6817f65e874cadf10
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-02-26 08:43:25 +0530

    Update afk_sql.py

--- MikuXProBot/modules/sql/afk_sql.py
+++ MikuXProBot/modules/sql/afk_sql.py
@@ -1,7 +1,9 @@
 import threading
 
+from sqlalchemy import Column, UnicodeText, Boolean, BigInteger
+
 from MikuXProBot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, BigInteger, UnicodeText
+
 
 class AFK(BASE):
     __tablename__ = "afk_users"
@@ -30,10 +32,9 @@ def is_afk(user_id):
 
 
 def check_afk_status(user_id):
-    try:
-        return SESSION.query(AFK).get(user_id)
-    finally:
-        SESSION.close()
+    if user_id in AFK_USERS:
+        return True, AFK_USERS[user_id]
+    return False, ""
 
 
 def set_afk(user_id, reason=""):
@@ -43,6 +44,7 @@ def set_afk(user_id, reason=""):
             curr = AFK(user_id, reason, True)
         else:
             curr.is_afk = True
+            curr.reason = reason
 
         AFK_USERS[user_id] = reason
 
@@ -63,19 +65,6 @@ def rm_afk(user_id):
 
         SESSION.close()
         return False
-
-
-def toggle_afk(user_id, reason=""):
-    with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
-        if not curr:
-            curr = AFK(user_id, reason, True)
-        elif curr.is_afk:
-            curr.is_afk = False
-        elif not curr.is_afk:
-            curr.is_afk = True
-        SESSION.add(curr)
-        SESSION.commit()
 
 
 def __load_afk_users():
