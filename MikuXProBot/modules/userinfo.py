61560b31b9119c63702d41f04e4f85334893bcd4
Tofu <91769994+Aditya6321@users.noreply.github.com>
2022-02-27 19:05:51 +0530

    Update userinfo.py

--- MikuXProBot/modules/userinfo.py
+++ MikuXProBot/modules/userinfo.py
@@ -269,7 +269,7 @@ def info(update: Update, context: CallbackContext):
     else:
         return
 
-    rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
+    rep = message.reply_text("<code>Preparing...</code>", parse_mode=ParseMode.HTML)
 
     text = (
         f"╒═══「<b>• Appraisal results •</b> 」\n"
@@ -447,21 +447,21 @@ def stats(update, context):
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
     botuptime = get_readable_time((time.time() - StartTime))
     status = "*╒═══「 System statistics 」*\n\n"
-    status += "*➢ System Start time:* " + str(uptime) + "\n"
+    status += "*➜ System Start time:* " + str(uptime) + "\n"
     uname = platform.uname()
-    status += "*➢ System:* " + str(uname.system) + "\n"
-    status += "*➢ Node name:* " + escape_markdown(str(uname.node)) + "\n"
-    status += "*➢ Release:* " + escape_markdown(str(uname.release)) + "\n"
-    status += "*➢ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
+    status += "*➜ System:* " + str(uname.system) + "\n"
+    status += "*➜ Node name:* " + escape_markdown(str(uname.node)) + "\n"
+    status += "*➜ Release:* " + escape_markdown(str(uname.release)) + "\n"
+    status += "*➜ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
     mem = virtual_memory()
     cpu = cpu_percent()
     disk = disk_usage("/")
-    status += "*➢ CPU:* " + str(cpu) + " %\n"
-    status += "*➢ RAM:* " + str(mem[2]) + " %\n"
-    status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
-    status += "*➢ Python Version:* " + python_version() + "\n"
-    status += "*➢ python-Telegram-Bot:* " + str(ptbver) + "\n"
-    status += "*➢ Uptime:* " + str(botuptime) + "\n"
+    status += "*➜ CPU:* " + str(cpu) + " %\n"
+    status += "*➜ RAM:* " + str(mem[2]) + " %\n"
+    status += "*➜ Storage:* " + str(disk[3]) + " %\n\n"
+    status += "*➜ Python Version:* " + python_version() + "\n"
+    status += "*➜ python-Telegram-Bot:* " + str(ptbver) + "\n"
+    status += "*➜ Uptime:* " + str(botuptime) + "\n"
     try:
         update.effective_message.reply_photo(
             MIKU_IMG,
