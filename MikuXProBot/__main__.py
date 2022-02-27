7be7094909f0331bd6dd66f0d417186f8fed28a4
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-02-27 10:05:04 +0530

    Update __main__.py

--- MikuXProBot/__main__.py
+++ MikuXProBot/__main__.py
@@ -210,12 +210,7 @@ def start(update: Update, context: CallbackContext):
         id = update.effective_user.id
         chat = update.effective_chat.title
         update.effective_message.reply_photo(
-                random.choice(MIKU_IMG), TEXXT.format(
-                first,
-                id,
-                chat,
-                uptime
-            ),
+                random.choice(MIKU_IMG), caption=TEXXT,
 
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
