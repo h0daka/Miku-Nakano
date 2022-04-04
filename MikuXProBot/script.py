b45b59b4e16fe599af4b85b5b30b7acb82157e33
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-04 08:38:03 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -1,4 +1,4 @@
-PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @ShukurenaiPRobot For Repo"""
+PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\nThanks To @MikuXXProBot For Repo"""
 
 MIKU_IMG = (
       "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
