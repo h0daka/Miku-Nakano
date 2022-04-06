95c99691640ca2c239668ab70dfb0e0a1963c881
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-06 21:25:13 +0530

    Update __init__.py

--- MikuXProBot/__init__.py
+++ MikuXProBot/__init__.py
@@ -235,7 +235,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from MikuXProBot.modules.helper_funcs.handlers import (CustomCommandHandler,
+from ShukurenaiPRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
