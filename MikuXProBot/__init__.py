394baa840ffbf7daf1c21e7868afc986baf97ee4
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-06 21:58:11 +0530

    Update __init__.py

--- MikuXProBot/__init__.py
+++ MikuXProBot/__init__.py
@@ -256,7 +256,7 @@ BOT_USERNAME = bottie.username
 BOT_NAME = bottie.first_name
 BOT_MENTION = bottie.mention
 
-if @MikuXProBot" not in PM_START_TEXT:
-    LOGGER.critical(f"{OWNER_ID} Is Cheating. Add `Thanks To @MikuXProBot For Repo` In PM_START_TEXT To Fix This")
+if "@ShukurenaiPRobot" not in PM_START_TEXT:
+    LOGGER.critical(f"{OWNER_ID} Is Cheating. Add `Thanks To @Shukurenai For Repo` In PM_START_TEXT To Fix This")
 else:
     LOGGER.info("Your Bot Is Ready")
