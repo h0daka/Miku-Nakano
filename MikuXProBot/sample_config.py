053d6bac744338f6089558a57a0095fe47c3e8c5
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-06 16:53:14 +0530

    Update sample_config.py

--- MikuXProBot/sample_config.py
+++ MikuXProBot/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = "awoo"  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5077976227 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "awoo"
-    SUPPORT_CHAT = 'mikussupport'  #Your own group for support, do not add the @
+    SUPPORT_CHAT = 'SHUKURENAI_SUPPORT'  #Your own group for support, do not add the @
     JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
@@ -57,6 +57,8 @@ class Config(object):
     AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    
+
 
 
 class Production(Config):
