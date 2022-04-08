03760ff22ecc7640756390b48d68ea5d7fcd347f
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-08 13:02:36 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -5,21 +5,20 @@ MIKU_IMG = "https://telegra.ph//file/7646aaad2c896f1832ac2.jpg",
 
 
 MIKU_IMG = (
-      "https://telegra.ph/file/3f06de01df5bc3c3cf343.jpg",
-      "https://telegra.ph//file/05311caed13176f7d35e5.jpg",
-      "https://telegra.ph//file/88976abda0d0af9d4a517.jpg",
-      "https://telegra.ph//file/b388f473ddfb9cc727bb1.jpg",
-      "https://telegra.ph//file/d950844dad2e473950470.jpg",
+      "https://telegra.ph//file/b716bc386c81048ff681f.jpg",
+      "https://telegra.ph//file/e8caba1ebf38cd9ff1e58.jpg",
+      "https://telegra.ph//file/10c8da788e3df7885ee52.jpg",
+      "https://telegra.ph//file/d380a6f3ba697c8d3faae.jpg",
 )
 
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
 
 MIKU_N_IMG = (
       "https://telegra.ph/file/837c61d9c51236fea4100.jpg",
-      "https://telegra.ph/file/ee34cf0d7e4782424b777.jpg",
-      "https://telegra.ph/file/5410b02359a2cabc2776b.jpg",
-      "https://telegra.ph/file/b1fc3b2af759999bf3b35.jpg",
-      "https://telegra.ph/file/305b5b3c4527cd439b926.jpg"
+      "https://telegra.ph//file/b716bc386c81048ff681f.jpg",
+      "https://telegra.ph//file/e8caba1ebf38cd9ff1e58.jpg",
+      "https://telegra.ph//file/10c8da788e3df7885ee52.jpg",
+      "https://telegra.ph//file/d380a6f3ba697c8d3faae.jpg"
 
 )
 
