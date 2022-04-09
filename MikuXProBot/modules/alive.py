c68b64fb72b08f09bf236d0f3da4fcdb60d73513
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-09 18:19:16 +0530

    Update alive.py

--- MikuXProBot/modules/alive.py
+++ MikuXProBot/modules/alive.py
@@ -56,7 +56,7 @@ async def hmm(yes):
     Miku += f"♡ **Telethon Version** ~♪ `{version.__version__}`\n\n"
     Miku += f"♡ **Python Telegram Bot Version** ~♪ `{telegram.__version__}`\n\n"
     Miku += f"♡ **Pyrogram Version** ~♪ `{pyro}`\n\n"
-    Miku += f"♡ **My Master** ~♪ [https://t.me/Sivatheboss](tg://user?id={OWNER_ID})\n\n"
+    Miku += f"♡ **My Master** ~♪ [https://t.me/{Sivatheboss}](tg://user?id={OWNER_ID})\n\n"
     Miku += f"Thanks For Adding Me In {yes.chat.title}"
     BUTTON = [[Button.url("Support Chat", "https://t.me/SHUKURENAI_SUPPORT"), Button.url("Updates Channel", "https://t.me/SHUKURENAI_UPDATES")]]
     on = await borg.send_file(yes.chat_id, file=file2,caption=Miku, buttons=BUTTON)
