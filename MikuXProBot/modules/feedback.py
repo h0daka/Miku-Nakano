fd578a6641609175266993f6c6f21e98e3685e08
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-10 21:40:00 +0530

    Update feedback.py

--- MikuXProBot/modules/feedback.py
+++ MikuXProBot/modules/feedback.py
@@ -28,7 +28,7 @@ async def feedback(e):
     user_id = e.sender.id
     user_name = e.sender.first_name
     mention = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-    BUTTON = [[Button.url("Go To Support Group", f"https://t.me/SENKUCHAT")]]
+    BUTTON = [[Button.url("Go To Support Group", f"https://t.me/SHUKURENAI_SUPPORT")]]
     TEXT = "Thanks For Your Feedback, I Hope You Happy With Our Service"
     GIVE = "Give Some Text For Feedback ✨"
     logger_text = f"""
