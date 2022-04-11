1c490d421f033d0600945b7ab681abf5d1a78790
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-11 13:12:40 +0530

    Update telegraph.py

--- MikuXProBot/modules/telegraph.py
+++ MikuXProBot/modules/telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-miku = "MIKU"
+shu = "SHU"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=miku)
 auth_url = r["auth_url"]
