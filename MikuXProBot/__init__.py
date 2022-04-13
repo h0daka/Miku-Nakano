85da2c4278a850e59a820661a7cf7e0e77b833c3
Sivatheboss88 <101733376+Sivatheboss88@users.noreply.github.com>
2022-04-13 18:36:43 +0530

    Update __init__.py

--- MikuXProBot/__init__.py
+++ MikuXProBot/__init__.py
@@ -24,7 +24,7 @@ logging.basicConfig(
               logging.StreamHandler()],
     level=logging.INFO)
 
-LOGGER = logging.getLogger("[ShukurenaiPRobot]")
+LOGGER = logging.getLogger("[MikuXProbot,ShukurenaiPRobot]")
 
 log = logging.getLogger('[Your Bot Is Building]')
 
