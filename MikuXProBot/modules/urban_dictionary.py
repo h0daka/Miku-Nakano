a6605e9a3d8768a83beeffc476d8bc0808f6906f
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-06-07 14:29:50 +0530

    Updated urban dictionary module

--- MikuXProBot/modules/urban_dictionary.py
+++ MikuXProBot/modules/urban_dictionary.py
@@ -1,24 +1,34 @@
 import requests
 from MikuXProBot import dispatcher
 from MikuXProBot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
+                      Update)
+from telegram.ext import CallbackContext
 
 
-@run_async
 def ud(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text[len('/ud '):]
     results = requests.get(
         f'https://api.urbandictionary.com/v0/define?term={text}').json()
     try:
-        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+        reply_text = f'*⚠️ Warning:* Urban Dictionary does not always provide accurate descriptions\n\n*• Word :* `{text}`\n\n*• Meaning :* _{results["list"][0]["definition"]}\n_\n*• Example:* _{results["list"][0]["example"]}_'
     except:
         reply_text = "No results found."
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                  [                  
+                       InlineKeyboardButton(
+                             text="🔎 Google it!",
+                             url=f"https://www.google.com/search?q=define+{text}")
+                     ] 
+                ]
+            ),
+         )
 
 
-UD_HANDLER = DisableAbleCommandHandler(["ud"], ud)
+UD_HANDLER = DisableAbleCommandHandler(["ud"], ud, run_async=True)
 
 dispatcher.add_handler(UD_HANDLER)
 
